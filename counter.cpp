2638f3a191c3d0c4e504afa2b27858495f4fe508
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-11-23 18:42:10 +0300

    first commit

---
+++ counter.cpp
@@ -0,0 +1,8 @@
+//
+//  counter.cpp
+//  cppm-homework-06.02
+//
+//  Created by a1ex on 11/22/23.
+//
+
+#include "counter.hpp"
---
+++ counter.hpp
@@ -0,0 +1,13 @@
+//
+//  counter.hpp
+//  cppm-homework-06.02
+//
+//  Created by a1ex on 11/22/23.
+//
+
+#ifndef counter_hpp
+#define counter_hpp
+
+#include <stdio.h>
+
+#endif /* counter_hpp */
